087a3247abcbcc7b170032a09cd4a3361f0b9be3
hippo321-0 <porapo789@gmail.com>
2025-04-02 14:11:07 +0530

    Update team8_problem2.py

--- team8_problem2.py
+++ team8_problem2.py
@@ -1,8 +1,10 @@
-a={"xiv":14 , "xx":20 ," ix" : 9, "v" :5, "xl":40, "x":10, "c" :100 , "cd":400 , "m" :1000,"d": 500 ,"xc" :90,"cm": 900, "md":1500, "xxxiv":34 }
-def sum(self,rom1,rom2):
+a={"xiv":14 , "xx":20 ," ix": 9, "v":5, "xl":40, "x":10, "c":100 , "cd":400 , "m":1000,"d":500 ,"xc":90,"cm": 900, "md":1500, "xxxiv":34 }
+def sum(rom1,rom2):
     num1=a[rom1]
     num2=a[rom2]
     result=num1+num2
     print(result)
-add=sum(xiv,xx)
 
+ip1=input("Enter first roman number")
+ip2=input("Enter second roman number")
+sum(ip1,ip2)
